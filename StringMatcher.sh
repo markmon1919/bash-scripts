040369573d0b4616e8d68156307d039e4d0ec22b
Mark Mon Monteros <broodtech@gmail.com>
2018-07-27 21:39:27 +0800

    Update StringMatcher.sh

--- StringMatcher.sh
+++ StringMatcher.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo -e "\n+----------+----------+"
-echo -e "String Match Automation \n  by Mark Mon Monteros"
-echo -e "+----------+----------+\n"
+echo -e "\n+-----------+-----------+"
+echo -e " String Match Automation \n  by Mark Mon Monteros"
+echo -e "+-----------+-----------+\n"
 
-declare mainSource=(`cat ~/Desktop/mainSource.csv | awk '{print$1}'`)
-declare getFileRow1=(`cat ~/Desktop/file.csv | awk '{print$1}'`)
-declare getFileRow2=(`cat ~/Desktop/file.csv | awk '{print$2}'`)
-echo "${mainSource[0]} ${getFileRow1[0]}" > ~/Desktop/output.csv
+declare mainSource=(`cat ~/Desktop/mainSource.csv | awk '{print$1}'`)	#Main Source File
+declare getFileCol1=(`cat ~/Desktop/file.csv | awk '{print$1}'`)		#File Match Column-1
+declare getFileCol2=(`cat ~/Desktop/file.csv | awk '{print$2}'`)		#File Match Column-2
+echo "${mainSource[0]} ${getFileCol1[0]}" > ~/Desktop/output.csv
 
 for (( i=1; i<${#mainSource[*]}; i++ )); do
-        count=i;
-        if [[ i -eq $count ]]; then
-                for (( j=1; j<${#getFileRow2[*]}; j++ )); do
-                        if [[ ${mainSource[$count]} == ${getFileRow2[j]} ]]; then
-                                echo "${mainSource[i]}" "${getFileRow1[j]}" >> ~/Desktop/output.csv
-                        fi
-                done
-        fi
-        count+=1;
+	count=i;
+	if [[ i -eq $count ]]; then		
+		for (( j=1; j<${#getFileCol2[*]}; j++ )); do
+			if [[ ${mainSource[$count]} == ${getFileCol2[j]} ]]; then
+				echo "${mainSource[i]}" "${getFileCol1[j]}" >> ~/Desktop/output.csv
+			fi
+		done
+	fi
+	count+=1;
 done
 
 cat ~/Desktop/output.csv
